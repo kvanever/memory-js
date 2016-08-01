9327ec0549fbfedaa1984b3f1814bcc8a06f1093
Kevin VanEvery and Matthew Giem <kevin+matthew@>
2016-08-01 15:18:01 -0700

    complete the memory project

--- .build/js/app.js
+++ .build/js/app.js
@@ -1,3 +1,33 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function Memory(number) {
+  this.number = number;
+  this.array = [];
+}
+
+Memory.prototype.shuffle = function() {
+  var counter = this.array.length;
+  while (counter > 0) {
+      var index = Math.floor(Math.random() * counter);
+      counter--;
+      var temp = this.array[counter];
+      this.array[counter] = this.array[index];
+      this.array[index] = temp;
+  }
+  return this.array;
+};
+
+Memory.prototype.createArray = function()  {
+  for(i = 1; i <= this.number; i++) {
+    this.array.push(i);
+    this.array.push(i);
+  }
+  return this.array;
+};
+
+
+exports.memoryModule = Memory;
+
+},{}],2:[function(require,module,exports){
 var Memory = require('./../js/memory-model.js').memoryModule;
 
 $(document).ready(function() {
@@ -37,3 +67,5 @@ $(document).ready(function() {
     }
   });
 });
+
+},{"./../js/memory-model.js":1}]},{},[2]);
--- js/memory-model.js
+++ js/memory-model.js
@@ -1,1 +1,27 @@
+function Memory(number) {
+  this.number = number;
+  this.array = [];
+}
+
+Memory.prototype.shuffle = function() {
+  var counter = this.array.length;
+  while (counter > 0) {
+      var index = Math.floor(Math.random() * counter);
+      counter--;
+      var temp = this.array[counter];
+      this.array[counter] = this.array[index];
+      this.array[index] = temp;
+  }
+  return this.array;
+};
+
+Memory.prototype.createArray = function()  {
+  for(i = 1; i <= this.number; i++) {
+    this.array.push(i);
+    this.array.push(i);
+  }
+  return this.array;
+};
+
+
 exports.memoryModule = Memory;
